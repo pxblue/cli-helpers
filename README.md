b837fd80e70e41ee585a4a28bb85a7bd447af9e0
joebochill <boyle.p.joseph@gmail.com>
2020-04-08 07:53:41 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# PX Blue CLI Helpers
+This repository contains helper files for the [PX Blue CLI](https://www.npmjs.com/package/@pxblue/cli).
+
+As of now, it includes:
+- Open Sans font files
+- React Native App template files
---
+++ react-native/expo/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from '@use-expo/font';
+import { ThemeProvider } from '@pxblue/react-native-components';
+import * as PXBThemes from '@pxblue/react-native-themes';
+import { AppLoading } from 'expo';
+
+export default function App() {
+    const [fontsLoaded] = useFonts({
+        'OpenSans-ExtraBold': require('./assets/fonts/OpenSans-ExtraBold.ttf'),
+        'OpenSans-Bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+        'OpenSans-SemiBold': require('./assets/fonts/OpenSans-SemiBold.ttf'),
+        'OpenSans-Regular': require('./assets/fonts/OpenSans-Regular.ttf'),
+        'OpenSans-Light': require('./assets/fonts/OpenSans-Light.ttf'),
+    });
+    if (!fontsLoaded) {
+        return <AppLoading />;
+    }
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
