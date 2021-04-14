482e9786fcc56bdbe6075c85d723a15b3494bc1c
Joseph Boyle <boyle.p.joseph@gmail.com>
2021-04-14 07:20:55 -0400

    Update App.js

--- react-native/expo/App.js
+++ react-native/expo/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { useFonts } from '@use-expo/font';
+import { useFonts } from 'expo-font';
 import { Provider as ThemeProvider } from 'react-native-paper';
 import * as PXBThemes from '@pxblue/react-native-themes';
 import AppLoading from 'expo-app-loading';
